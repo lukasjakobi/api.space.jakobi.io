206c36cd7800a7e687d665b07b2c593115620eea
Lukas Jakobi <lukas.jakobi@check24.de>
2020-12-30 14:04:14 +0100

    - minor bug fixes

---
+++ app/Http/Managers/StatusManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Managers;
+
+use App\Models\LaunchStatus;
+use App\Models\Rocket;
+use Illuminate\Support\Facades\DB;
+
+class StatusManager
+{
+    private const TABLE = "rl_status";
+
+    private const SELECT = [
+        "id", "displayName", "cancelled"
+    ];
+
+    /**
+     * @param int $id
+     * @return Rocket|null
+     */
+    public function getStatusById(int $id): ?LaunchStatus
+    {
+        $result = DB::table(self::TABLE)
+            ->select(self::SELECT)
+            ->where("id", "=", $id)
+            ->first();
+
+        if ($result === null) {
+            return null;
+        }
+
+        return $this->buildStatusFromDatabaseResult($result);
+    }
+
+    /**
+     * @param string $slug
+     * @return Rocket|null
+     */
+    public function getStatusBySlug(string $slug): ?LaunchStatus
+    {
+        $result = DB::table(self::TABLE)
+            ->select(self::SELECT)
+            ->where("slug", "=", $slug)
+            ->first();
+
+        if ($result === null) {
+            return null;
+        }
+
+        return $this->buildStatusFromDatabaseResult($result);
+    }
+
+    public function getTotalAmount(): int
+    {
+        return DB::table(self::TABLE)->selectRaw("COUNT(*) as total")->first()->total ?? 0;
+    }
+
+    /**
+     * @param $result
+     * @return LaunchStatus
+     */
+    private function buildStatusFromDatabaseResult($result): LaunchStatus
+    {
+        $launchStatus = new LaunchStatus();
+
+        if (isset($result->id)) {
+            $launchStatus->setId($result->id);
+        }
+
+        if (isset($result->displayName)) {
+            $launchStatus->setDisplayName($result->displayName);
+        }
+
+        if (isset($result->cancelled)) {
+            $launchStatus->setCancelled($result->cancelled);
+        }
+
+        return $launchStatus;
+    }
+}
